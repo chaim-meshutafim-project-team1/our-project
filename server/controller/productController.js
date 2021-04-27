05561e7682d6ffab4845d2a780184443e13b3779
EyalIlan <eyalilan8@gmail.com>
2021-04-27 15:11:12 +0300

    controller

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -1,18 +1,23 @@
 
 const addProduct = (req,res) =>{
     
-}
 
+}
 
 const deleteProduct = (req,res) =>{
 
 
 }
 
+
 const updateProduct = (req,res) =>{
+
+
 
 }
 const readProduct = (req,res) =>{
+
+    
 
 }
 

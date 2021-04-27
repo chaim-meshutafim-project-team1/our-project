69e4bc6a71769852700f3fc2f195983b0ce3bdae
David Bobritsky <dvdbobr@gmail.com>
2021-04-27 13:25:31 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,8 +8,9 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // app.use('/api/users', userRouter)
-app.get('/', (req, res) => {
-    res.json({ success: { id: 1, email: 'asfasf@asfasf.com' } })
+app.get('/api/translate', (req, res) => {
+    const { url } = req.body;
+    res.json({ success: { msg: "hello", url: url } })
 })
 app.listen(process.env.PORT || 5000, () => {
     console.log(`application start at ${process.env.PORT || 5000}`)

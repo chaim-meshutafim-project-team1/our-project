4b435a86a2c574cf0a23d6776a3c9e121ff98a25
orisailes <ori1sailes@gmail.com>
2021-04-27 22:19:31 +0300

    Update scraper.js

--- server/controller/scraper.js
+++ server/controller/scraper.js
@@ -9,18 +9,18 @@ const yad2Scraper = async (url) => {
             waitUntil: 'networkidle2'
         });
 
-        // const description = await page.evaluate(() => {
-        //     const result = document.querySelector('#teurWrap').textContent;
-        //     return result;
-        // });
-        // const title = await page.evaluate(() => {
-        //     const result = document.querySelector('.addressTop>h1>span').textContent
-        //     return result
-        // })
-        // const price = await page.evaluate(() => {
-        //     const result = document.querySelector('.ModaaWDetailsValue').textContent
-        //     return result
-        // })
+        const description = await page.evaluate(() => {
+            const result = document.querySelector('#teurWrap').textContent;
+            return result;
+        });
+        const title = await page.evaluate(() => {
+            const result = document.querySelector('.addressTop>h1>span').textContent
+            return result
+        })
+        const price = await page.evaluate(() => {
+            const result = document.querySelector('.ModaaWDetailsValue').textContent
+            return result
+        })
         const image = await page.evaluate(() => {
             const result = document.querySelector('.tmunotDesktop__img-container>img')
             return result.buffer()
@@ -28,17 +28,20 @@ const yad2Scraper = async (url) => {
 
         await browser.close();
 
-            console.log(image)
-        // let helper ={
-        //     image: image,
-        //     price: price,
-        //     title: title.replace(/\n/g,'').split('').reverse().join(''),
-        //     description: description.split('').reverse().join(''),
-        //     lastUpdate:new Date(),
-        // }
-        // console.log(helper);
+           
+        let helper ={
+            image: image,
+            price: price,
+            title: title.replace(/\n/g,'').split('').reverse().join(''),
+            description: description.split('').reverse().join(''),
+            lastUpdate:new Date(),
+        }
+
+       return helper
+       
     } catch (error) {
         console.log('error runnignthe func', error);
     }
 }
-console.log(yad2Scraper(urlToScrape));
+
+module.exports= yad2Scraper

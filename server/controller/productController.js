e44c5c256e9159151c6129bb4b08af19eb53c46c
ChenHzt <chenhzt@gmail.com>
2021-04-27 22:17:14 +0300

    Update productController.js

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -1,19 +1,18 @@
 const { Product } = require('../models/product.model.js')
 const CheckDay = require('../utils/daysPassSince')
+
 const translate = require('../aws/translate')
 const addProduct = async (productObj) => {
     const { productID, productImg, price, title, description, date, productState,url } = productObj
     try {
         
         const product = new Product({
-            url:url,
             productID: productID,
             image: productImg,
             price: price,
             title: title,
             description: description,
-            lastUpdate:date,
-            productState: productState
+            lastUpdate:date
         });
         await product.save()
         return product
@@ -31,12 +30,10 @@ const updateProduct = async (productObj) => {
 
     const product = await Product.updateOne({ productID:productObj.productID }, {
         $set: {
-            url,
             price,
             title,
             description,
             date,
-            productState,
             imageUrl
         }
     }, { new: true, runValidators: true })
@@ -61,29 +58,28 @@ const readProduct = async (productID) => {
 
 const GetTranslate = async (req, res) => {
     // const { url } = req.body;
-    const url = "www.google.co.il/132122834";
+    const url = "www.google.co.il/132122234";
     const id =  url.substring(url.lastIndexOf('/') + 1)
 
 
 
     const product = await readProduct(id)
 
-        const Scrape = {
-            productID:id,
-            url:url,  
-            price:200,
-            title:'headling',
-            description:'hello',
-            lastUpdate:new Date(),
-            productState:'new',
-            image:'https://images.pexels.com/photos/1590901/pexels-photo-1590901.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
-        }
+        // const Scrape = {
+        //     productID:id,
+        //     price:200,
+        //     title:'headling',
+        //     description:'hello',
+        //     lastUpdate:new Date(),
+        //     image:'https://images.pexels.com/photos/1590901/pexels-photo-1590901.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+        // }
         
+        
+
        let AddedProduct = await addProduct(Scrape);
-        const fieldsToTranslate = ['title','description','productState'];
+        const fieldsToTranslate = ['title','description'];
         const translated = {}; 
         translated.productID = AddedProduct.productID;
-        translated.url = AddedProduct.url;
         translated.price = AddedProduct.price;
         translated.lastUpdate = AddedProduct.lastUpdate;
         translated.image = AddedProduct.image;

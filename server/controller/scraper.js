1992edf0150d7ec553870b345c7ebbb41d666096
Ori Sailes <77921279+orisailes@users.noreply.github.com>
2021-04-27 22:16:50 +0300

    Update scraper.js

--- server/controller/scraper.js
+++ server/controller/scraper.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer');
-const urlToScrape = 'https://www.komo.co.il/code/y2/details/?modaaNum=2681958';
+
 const yad2Scraper = async (url) => {
     try {
         const browser = await puppeteer.launch();
@@ -9,18 +9,18 @@ const yad2Scraper = async (url) => {
             waitUntil: 'networkidle2'
         });
 
-        // const description = await page.evaluate(() => {
-        //     const result = document.querySelector('#teurWrap').textContent;
-        //     return result;
-        // });
-        // const title = await page.evaluate(() => {
-        //     const result = document.querySelector('.addressTop>h1>span').textContent
-        //     return result
-        // })
-        // const price = await page.evaluate(() => {
-        //     const result = document.querySelector('.ModaaWDetailsValue').textContent
-        //     return result
-        // })
+        const description = await page.evaluate(() => {
+            const result = document.querySelector('#teurWrap').textContent;
+            return result;
+        });
+        const title = await page.evaluate(() => {
+            const result = document.querySelector('.addressTop>h1>span').textContent
+            return result
+        })
+        const price = await page.evaluate(() => {
+            const result = document.querySelector('.ModaaWDetailsValue').textContent
+            return result
+        })
         const image = await page.evaluate(() => {
             const result = document.querySelector('.tmunotDesktop__img-container>img')
             return result.buffer()
@@ -29,16 +29,17 @@ const yad2Scraper = async (url) => {
         await browser.close();
 
             console.log(image)
-        // let helper ={
-        //     image: image,
-        //     price: price,
-        //     title: title.replace(/\n/g,'').split('').reverse().join(''),
-        //     description: description.split('').reverse().join(''),
-        //     lastUpdate:new Date(),
-        // }
-        // console.log(helper);
+        let helper ={
+            image: image,
+            price: price,
+            title: title.replace(/\n/g,'').split('').reverse().join(''),
+            description: description.split('').reverse().join(''),
+            lastUpdate:new Date(),
+        }
+        console.log(helper);
     } catch (error) {
         console.log('error runnignthe func', error);
     }
 }
-console.log(yad2Scraper(urlToScrape));
+
+module.exports= yad2Scraper

fc95975c480219959458ffeaa492b77e8459bdb4
Hila Kershenovich <hilawol@gmail.com>
2021-04-27 15:30:34 +0300

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import Landing from './components/pages/landing'
 import Translation from './components/pages/translation'
 import Favorites from './components/pages/favorites'
 import './components/pages/css/normalize.css'
+import Navbar from './components/utils/Navbar'
 
 function App() {
 
   return (
     <BrowserRouter>
+    <Navbar/>
       <Switch>
       <Route exact path="/" component={Landing}/>
       <Route exact path="/translation" component={Translation}/>

21156c7e8b9bbdfe81ab50d8759cd3238f63d97c
orisailes <ori1sailes@gmail.com>
2021-04-27 16:56:51 +0300

    front end update

--- client/src/components/pages/favorites.jsx
+++ client/src/components/pages/favorites.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './css/favorites.css'
+import Card from '../utils/Card'
 
 const Favorites = () => {
-    return(
+
+    const [favoritesItems, setFavoritesItems] = useState([])
+
+    useEffect(() => {
+        const favorites = JSON.parse(localStorage.getItem('favorites')) || []
+        setFavoritesItems(favorites)
+    }, [])
+
+    const deleteItem = () => {
+        console.log('deleting.....');
+    }
+
+    return (
         <div className="favorites-page">
-            favorites page
+            <div className="favorites-container">
+                {favoritesItems.length ?
+                    favoritesItems.map((item) => {
+                        return (
+                            <div>
+                                <Card item={item} />
+                                <i onClick={deleteItem} class="far fa-trash-alt fa-2x"></i>
+                            </div>
+                        )
+                    })
+                    :
+                    <div>
+                        <h1>No favorites</h1>
+                    </div>
+                }
+            </div>
         </div>
     )
 }
--- client/src/components/pages/translation.jsx
+++ client/src/components/pages/translation.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import './css/translation.css'
 import Button from '../utils/Button'
 import TextInput from '../utils/TextInput'
+import Card from '../utils/Card'
+import DropDown from '../utils/DropDown'
 import axios from 'axios'
 
 const Translation = () => {
 
     const [inputValue, setInputValue] = useState('')
-    const [card, setCard] = useState([])
+    const [card, setCard] = useState(null)
 
     const handleChange = (e) => {
         setInputValue(e.target.value)
@@ -22,12 +24,30 @@ const Translation = () => {
         // temp.push(result.data)
     }
 
+    const addToFav = () => {
+        console.log('adding to fav....');
+    }
 
     return (
         <div className="translation-page">
-            <div>Nav bar here</div>
-            <div><TextInput type="text" value={inputValue} onChange={(e) => handleChange(e)} placeholder='Insert "Yad2" URL here' /></div>
-            <div><Button onClick={sendURL} text="Submit" /></div>
+            <div className="search-container">
+                <TextInput type="text" value={inputValue} onChange={(e) => handleChange(e)} placeholder='Insert "Yad2" URL here' />
+                <DropDown/>
+                <Button onClick={sendURL} text="Submit" />
+            </div>
+            <div className="cards-container">
+                {card ?
+                    <div>
+                        <Card item={card} />
+                        <i onClick={addToFav} class="far fa-heart fa-2x"></i>
+                    </div>
+                    :
+                    <div>
+                        <h1>Insert URL</h1>
+                    </div>
+                }
+
+            </div>
         </div>
     )
 }

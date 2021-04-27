e07a88a2bf676d6b1a87039e2439abcaba55ab24
SaifKbishi <39298208+SaifKbishi@users.noreply.github.com>
2021-04-27 20:02:25 +0300

    scraper working

--- server/controller/scraper.js
+++ server/controller/scraper.js
@@ -1,28 +1,36 @@
 const { json } = require('body-parser');
 const puppeteer = require('puppeteer');
-
-const urlToScrape = 'https://www.yad2.co.il/item/a6ejvs'
-// const urlToScrape = 'https://gulshansainis.github.io/portfolio/'
+const urlToScrape = 'https://www.yad2.co.il/item/a6ejvs';
 
 const yad2Scraper = async (url)=>{
  try{
+  let prodObj={
+   title:'',   
+   description:'',
+   price:0,
+   
+  };
  const browser = await puppeteer.launch();
  const page = await browser.newPage();
+ await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
  await page.goto(url, {waitUntil: 'networkidle2'}); 
 
-  // const heading1 = await page.$eval("body > div > div > h1", el => el.textContent);
-  // const test1 = await page.evaluate(() => {   document.querySelector('#portfolio>h2') });
+  const description = await page.evaluate(() => {
+   const text = document.querySelector('.details_text').textContent; 
+   return text;
+  });
+  const  prodStatus = await page.evaluate(() => {
+   const description = document.querySelector('#important_item_status_0').textContent.trim();
+   return description;
+  });
+  //document.querySelector('.image_con>img').src  //image src
+  //document.querySelector('.price').textContent  //price
+  //document.querySelector('.content .title').textContent  //title
+  //document.querySelector('#important_item_updated_at_0').textContent.trim();  //updated
   
-  const test1 = await page.evaluate(() => {   document.querySelector('.details_text').textContent});
-  // const test2 = await page.evaluate(() => {   document.querySelector('.details_text').innerHTML });
-  // const test3 = await page.evaluate(() => {   document.querySelector('.details_text').innerText });
-  // console.log(test1,test2, test3);
-  console.log(test1)
-
- // console.log(title);
-
- await browser.close();
+  
+  await browser.close();
+  console.log(description, prodStatus);
  }catch(error){console.log('error runnignthe func', error);}
 }
-
 console.log(yad2Scraper(urlToScrape));
--- server/controller/scraper2.js
+++ server/controller/scraper2.js
@@ -1,32 +1,40 @@
 const { json } = require('body-parser');
 const puppeteer = require('puppeteer');
-
-const options = {
-  executablePath:
-    '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-  headless: false,
-  defaultViewport: null,
-  args: ['--window-size=1920,1080'],
-}
-
-const urlToScrape = 'https://www.yad2.co.il/item/a6ejvs'
+const urlToScrape = 'https://nadlan.ynet.co.il/items/6f364415-2f77-4e86-9d91-27213676edc9';
 
 const yad2Scraper = async (url)=>{
-  const browser = await puppeteer.launch(options)
-  const page = await browser.newPage()
-  await page.setDefaultNavigationTimeout(0)
-  await page.goto(url, {waitUntil: 'networkidle2'});
+ try{
+  let prodObj={
+    title:'',
+    details:'',
+  };
+ const browser = await puppeteer.launch();
+ const page = await browser.newPage();
+ await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+ await page.goto(url, {waitUntil: 'networkidle2'}); 
 
-  // const title = await page.title();
-  // console.info(`The title is: ${title}`);
-
-  const title = await page.$eval(".main_details > .sub_category_title> span:nth-child(0)", elem => elem.textContent);
-  // let title3 = JSON.stringify(title)
-  // let parsedObj = JSON.parse(title);
-  console.log(title)
-
-  await browser.close()
+  const prod = await page.evaluate(() => {
+   const title = document.querySelector('.address-title').textContent;
+   const description = document.querySelector('.comments.first').textContent;
+   prodObj.title = title;
+   prodObj.details = description;
+   return prodObj;
+  });
+  const  details = await page.evaluate(() => {
+   
+   return description;
+  });
+  // prodObj.title = title;
+  // prodObj.details = details;
+  //document.querySelector('.image_con>img').src  //image src
+  //document.querySelector('.price').textContent  //price
+  //document.querySelector('.content .title').textContent  //title
+  //document.querySelector('#important_item_updated_at_0').textContent.trim();  //updated
+  
+  
+  await browser.close();
+  // console.log(title, details);
+  console.log('Object here: ',prodObj);
+ }catch(error){console.log('error runnignthe func', error);}
 }
-
-
 console.log(yad2Scraper(urlToScrape));

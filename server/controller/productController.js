12605b70218242ab813146f4bdd93d89f5391966
ChenHzt <chenhzt@gmail.com>
2021-04-27 22:51:24 +0300

    Update productController.js

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -1,6 +1,6 @@
 const { Product } = require('../models/product.model.js')
 const CheckDay = require('../utils/daysPassSince')
-
+const yad2Scraper = require('./scraper');
 const translate = require('../aws/translate')
 const addProduct = async (productObj) => {
     const { productID, productImg, price, title, description, date, productState,url } = productObj
@@ -57,10 +57,10 @@ const readProduct = async (productID) => {
 
 
 const GetTranslate = async (req, res) => {
-    // const { url } = req.body;
-    const url = "www.google.co.il/132122234";
-    const id =  url.substring(url.lastIndexOf('/') + 1)
-
+    const { url } = req.body;
+    // const url = "www.google.co.il/132122234";
+    const id =  url.substring(url.lastIndexOf('modaaNum=') + 9)
+    console.log(id)
 
 
     const product = await readProduct(id)
@@ -74,24 +74,24 @@ const GetTranslate = async (req, res) => {
         //     image:'https://images.pexels.com/photos/1590901/pexels-photo-1590901.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
         // }
         
-        
+        const scrapedData = await yad2Scraper(url);
+        console.log("**",scrapedData);
 
-       let AddedProduct = await addProduct(Scrape);
+       let AddedProduct = await addProduct({...scrapedData,price:scrapedData.replace(/\D/g,''),productID:id});
         const fieldsToTranslate = ['title','description'];
         const translated = {}; 
         translated.productID = AddedProduct.productID;
         translated.price = AddedProduct.price;
         translated.lastUpdate = AddedProduct.lastUpdate;
         translated.image = AddedProduct.image;
-        console.log(AddedProduct);
+        // console.log(translated);
 
         try{
-            fieldsToTranslate.forEach((translateField) => translated[translateField] = translate('he','en',AddedProduct[translateField]));
+            fieldsToTranslate.forEach((translateField) => translated[translateField] = translate('he',req.body.language,AddedProduct[translateField]));
         }
         catch(e) {
             return res.json({error:e.message});
         }
-        // console.log('**',translated);
         res.json(translated);
 }
 

761af14e8a6ede2b44f029aa021d17850a7e96aa
Hila Kershenovich <hilawol@gmail.com>
2021-04-27 16:49:15 +0300

    changed navbar navigation

--- client/src/components/utils/Navbar.jsx
+++ client/src/components/utils/Navbar.jsx
@@ -1,18 +1,39 @@
-import React,{useState} from 'react'
-import { Link } from 'react-router-dom'
+import React,{useState,useEffect} from 'react'
+import { Link,useLocation } from 'react-router-dom'
 import './css/navbar.css'
 import Logo from '../../img/yad2Logo.png'
 
-function Navbar() {
+function Navbar(props) {
 
   const [selected,setSelected]=useState("home");
+
+  const location = useLocation();
+
+  useEffect(() => {
+    const currentPath = location.pathname;
+    // console.log(currentPath);
+    switch (currentPath){
+      case '/':
+        setSelected("home");
+        break;
+      case '/translation':
+        setSelected("translation");
+        break;
+      case '/favorites':
+        setSelected("favorites");
+        break;
+      default:
+    }
+  }, [location]);
 
   return (
     <div className="navbar">
       <img className="logo" src={Logo} alt="yad2logo"/>
-      <div className="navBtn" onClick={()=>setSelected("home")}><Link className={selected==="home"? "selected":''} to="/">Home</Link></div>
-      <div className="navBtn" onClick={()=>setSelected("favorites")}><Link className={selected==="favorites"? "selected":''}to="/favorites">Favorites</Link></div>
-      <div className="navBtn" onClick={()=>setSelected("translation")}><Link className={selected==="translation"? "selected":''}to="/translation">Translate</Link></div>
+      <div className="navBtns">
+         <div className="navBtn" ><Link className={selected==="home"? "selected":''} to="/">Home</Link></div>
+         <div className="navBtn" ><Link className={selected==="translation"? "selected":''}to="/translation">Translate</Link></div>
+         <div className="navBtn" ><Link className={selected==="favorites"? "selected":''}to="/favorites">Favorites</Link></div>
+      </div>
     </div>
   )
 }

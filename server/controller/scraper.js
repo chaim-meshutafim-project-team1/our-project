d094d4c41afb89dd9fdd7e1a6ae2cd62833bf5c9
orisailes <ori1sailes@gmail.com>
2021-04-28 09:07:52 +0300

    update

--- server/controller/scraper.js
+++ server/controller/scraper.js
@@ -1,8 +1,11 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 
 const yad2Scraper = async (url) => {
     try {
-        const browser = await puppeteer.launch({args:['--no-sandbox', '--disable-setuid-sandbox']});
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox']
+        })
         const page = await browser.newPage();
         await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
         await page.goto(url, {
